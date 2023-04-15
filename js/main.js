f40afd8690551afbb4f2719ac4fba5740e5e6abc
surajj0809 <surajvaghela3107@gmail.com>
2023-04-15 13:40:31 +0530

    fix counter issue

--- js/main.js
+++ js/main.js
@@ -61,26 +61,17 @@
   });
 
   // website counter
-  var counterContainer = document.querySelector(".website-counter");
-  var resetButton = document.querySelector("#reset");
-  var visitCount = localStorage.getItem("page_view");
+  const countEl = document.getElementById("count");
 
-  // Check if page_view entry is present
-  if (visitCount) {
-    visitCount = Number(visitCount) + 1;
-    localStorage.setItem("page_view", visitCount);
-  } else {
-    visitCount = 1;
-    localStorage.setItem("page_view", 1);
+  updateVisitCount();
+
+  function updateVisitCount() {
+    fetch("https://api.countapi.xyz/update/vishwakarmaply/bf704727-7a0c-42fe-958e-0ddf3c1e19aa/?amount=1")
+      .then((res) => res.json())
+      .then((res) => {
+        countEl.innerHTML = res.value;
+      });
   }
-  counterContainer.innerHTML = visitCount;
-
-  // Adding onClick event listener
-  resetButton.addEventListener("click", () => {
-    visitCount = 1;
-    localStorage.setItem("page_view", 1);
-    counterContainer.innerHTML = visitCount;
-  });
 
   // Portfolio isotope and filter
   var portfolioIsotope = $(".portfolio-container").isotope({
